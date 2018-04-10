b1e4acd5c27a2b79dc73425777275c08b3b0173a
sunruyu <670331973@qq.com>
2018-04-10 12:27:44 +0800

    chore(init): 项目架构初始化

---
+++ src/main/java/com/zhanwang/postrank/PostrankApplication.java
@@ -0,0 +1,12 @@
+package com.zhanwang.postrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostrankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostrankApplication.class, args);
+	}
+}
